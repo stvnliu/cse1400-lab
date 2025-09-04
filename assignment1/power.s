fe66b4d89b14ce05aa26ef49bc82032c97af8399
firegory <85395729+firegory@users.noreply.github.com>
2025-09-04 10:30:30 +0200

    Added comments to power.s

--- assignment1/power.s
+++ assignment1/power.s
@@ -2,12 +2,19 @@
 message:	.asciz "Hi, We're Steven Liu (NetID: stevenliu) and Egor Yapparov (NetID: eyapparov). This is our submission for Assignment 1: Powers\n"
 .global main
 main:
+	#!PROLOGUE!
 	pushq	%rbp		# push base pointer
 	movq	%rsp, %rbp	# copy stack pointer to base pointer
-	movq	$0, %rax
-	movq	$message, %rdi
+
+	#!ACTUAL PRINTNG!
+	movq	$0, %rax	#not always necessary, sets the ammount of extra arguments for printf(not sure about this one)
+	movq	$message, %rdi	#moving our text into the first parameter register
 	call	printf
-	movq	%rbp, %rsp
-	popq	%rbp
-	movq	$0, %rdi
+
+	#!EPILOGE!
+	movq	%rbp, %rsp	#pushing base pointer into stack pointer(discarding all the variables)
+	popq	%rbp	#returning to the previous stack position
+
+	#!EXITING THE PROGHRAM!
+	movq	$0, %rdi	#moving 0 error code into the first parameter register
 	call	exit
