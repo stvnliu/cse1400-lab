2b0b2a3e3caa6a38338b90fa4290753fc8a13773
steven@devbox <steven@devbox.local>
2025-09-04 07:24:33 +0000

    power function

--- assignment1/power.s
+++ assignment1/power.s
@@ -1,13 +1,36 @@
 .text
-message:	.asciz "Hi, We're Steven Liu (NetID: stevenliu) and Egor Yapparov (NetID: eyapparov). This is our submission for Assignment 1: Powers\n"
+message:	
+	.asciz "Hi, We're Steven Liu (NetID: stevenliu) and Egor Yapparov (NetID: eyapparov). This is our submission for Assignment 1: Powers\n"
+result:
+	.asciz "The result of the power calculation is: %d\n";
 .global main
 main:
 	pushq	%rbp		# push base pointer
-	movq	%rsp, %rbp	# copy stack pointer to base pointer
+	movq	%rsp, %rbp	# copy stack pointer to base pointer ??
 	movq	$0, %rax
 	movq	$message, %rdi
 	call	printf
-	movq	%rbp, %rsp
+	movq	%rbp, %rsp	# ??
+	movq	$2, %rdi
+	movq	$10, %rsi
+	call	power
+	movq	$result, %rdi
+	movq	%rax, %rsi
+	call	printf
+
+end:
 	popq	%rbp
 	movq	$0, %rdi
 	call	exit
+power:
+	pushq	%rbp
+	movq	%rdi, %rbx	# first parameter into rbx
+	movq	$1, %rax	# 1 (n^0) stored in rax
+	movq	%rsi, %rcx	# exponent into rcx
+power_1:imul	%rbx, %rax	# integer multiply
+	dec	%rcx
+	cmp	$0, %rcx
+	jne	power_1
+	popq	%rbp
+	ret
+
---
+++ assignment1/spec.c
@@ -0,0 +1,16 @@
+/* *
+* The pow s u b r o u t i n e c a l c u l a t e s powers
+* o f non-n e g a t i v e b a s e s and e x p o n e n t s .
+*
+* Arguments :
+*
+* b a s e = t h e e x p o n e n t i a l b a s e
+* exp = t h e exponent
+*
+* Return v a l u e : ’ b a s e ’ r a i s e d t o t h e power o f ’ exp ’ .
+*/
+int pow(int base, int exp) {
+	int total = 1;
+	// ...
+	return total;
+}
