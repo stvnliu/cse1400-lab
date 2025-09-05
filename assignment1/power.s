0579789ca5781f268352210841d451d70fbf6289
firegory <85395729+firegory@users.noreply.github.com>
2025-09-05 14:36:05 +0200

    Improved dtyle of comments

--- assignment1/power.s
+++ assignment1/power.s
@@ -3,18 +3,18 @@ message:	.asciz "Hi, We're Steven Liu (NetID: stevenliu) and Egor Yapparov (NetI
 .global main
 main:
 	#!PROLOGUE!
-	pushq	%rbp		# push base pointer
-	movq	%rsp, %rbp	# copy stack pointer to base pointer
+	pushq	%rbp			#push base pointer
+	movq	%rsp, %rbp		#copy stack pointer to base pointer
 
 	#!ACTUAL PRINTNG!
-	movq	$0, %rax	#not always necessary, sets the ammount of extra arguments for printf(not sure about this one)
+	movq	$0, %rax		#not always necessary, sets the ammount of extra arguments for printf(not sure about this one)
 	movq	$message, %rdi	#moving our text into the first parameter register
 	call	printf
 
 	#!EPILOGE!
-	movq	%rbp, %rsp	#pushing base pointer into stack pointer(discarding all the variables)
-	popq	%rbp	#returning to the previous stack position
+	movq	%rbp, %rsp		#pushing base pointer into stack pointer(discarding all the variables)
+	popq	%rbp			#returning to the previous stack position
 
 	#!EXITING THE PROGHRAM!
-	movq	$0, %rdi	#moving 0 error code into the first parameter register
+	movq	$0, %rdi		#moving 0 error code into the first parameter register
 	call	exit
